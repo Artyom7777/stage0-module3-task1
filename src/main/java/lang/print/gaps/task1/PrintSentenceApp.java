869875258cca6fefc8f7493f1d412a325cf47c96
Artyom7777 <iartiq@gmail.com>
2022-09-27 23:21:57 +0300

    Task have done

--- src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
+++ src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
@@ -2,7 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintSentenceApp {
     public static void main(String[] args) {
-        String printWord =  "This\nis\nmy\nfirst\nJava\nprogram";
-        System.out.println(printWord);
+        System.out.println("This\nis\nmy\nfirst\nJava\nprogram");
     }
 }
--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,7 +2,6 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        String printWord = "java\nis\nsmth";
-        System.out.println(printWord);
+        System.out.println("java" + "\nis" +"\nsmth");
     }
 }
