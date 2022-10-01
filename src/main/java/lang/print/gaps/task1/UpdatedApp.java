ee0af9879cdb189fcb13099b378e7764487246b1
Artyom7777 <iartiq@gmail.com>
2022-10-01 08:26:18 +0300

    Task have done

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -2,8 +2,6 @@ package lang.print.gaps.task1;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        String printWord =  "I'm updated\n" +
-                "can be compiled. And can write to console";
-        System.out.println(printWord);
+        System.out.println("I'm updated\n" + "can be compiled. And can write to console");
     }
 }
